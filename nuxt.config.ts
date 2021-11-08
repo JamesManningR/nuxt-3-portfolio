175a035378746146cdfe21aa885e1226bcfb6506
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2021-11-08 22:23:14 +0000

    Added Graph ql

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
   css: [
-    'water.css/out/dark.min.css'
+    'water.css/out/dark.min.css',
+    'css-skeletons'
   ],
+  build: {
+    transpile: ['@apollo/client']
+  }
 })
