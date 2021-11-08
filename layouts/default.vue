0c362c41b2e8a23102c7dd81d727eff5756ced74
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2021-11-08 22:22:42 +0000

    Swapped out app for layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+header
+  h2 Curious Digital
+slot
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 <template lang="pug">
-header
-  h1 Curious Digital
-section
-  h2 Projects 
+h1 Projects 
+
+main
+  suspense
+    template(#default)
+      ul
+        li(v-for="project in projects")
+          project(:project="project")
+    
+    template(#fallback)
+      ul
+        li(v-for="num in 5")
+          .skeleton.skeleton-youtube
 </template>
 
-<script lang="ts">
-import { defineNuxtComponent } from 'nuxt3'
-
+<script setup lang="ts">
+const { data: projects } = await useFetch<string, Project[]>('https://api-eu-central-1.graphcms.com/v2/ckvijjdt04rp401y08r77611s/master', {
+  method: "POST",
+  body: {
+    query: "query{projects{title,body,featured,date,skills,thumbnail{id}}}"
+  }
+})
 </script>
