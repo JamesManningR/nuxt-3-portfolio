c0321624b47183926145442a8fc8dfb22bc07a3c
JamesManningR <56916766+JamesManningR@users.noreply.github.com>
2021-11-08 22:22:53 +0000

    Added project props

--- components/Project.vue
+++ components/Project.vue
@@ -1,21 +1,21 @@
 <template lang="pug">
 article
   h3 {{ project.title }}
+  p {{ project.body }}
 </template>
 
 <script lang="ts">
-import { defineNuxtComponent } from 'nuxt3'
+import { defineNuxtComponent } from '#app'
 import { PropType } from 'vue';
 
 export default defineNuxtComponent({
   props: {
     project: {
-      type: Object as PropType<Project>
+      type: Object as PropType<Project>,
+      required: true
     }
   },
 
-  setup(props) {
-
-  },
+  setup(props) {},
 })
 </script>
